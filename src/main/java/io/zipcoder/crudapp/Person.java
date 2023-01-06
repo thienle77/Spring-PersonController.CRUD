13b02be23d9363ac2cc5b1eda905972fbb5b62fd
thienle77 <tle9677@gmail.com>
2023-01-06 00:25:06 -0500

    Person class made

--- src/main/java/io/zipcoder/crudapp/Person.java
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -1,4 +1,41 @@
 package io.zipcoder.crudapp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+    String firstName;
+    String lastName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
 }
